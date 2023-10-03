4654a4628a6f2cf536853f66bbbeca8144b0894f
Hatim <hatmsb11@gmail.com>
2023-10-03 23:59:19 +0530

    hatim commit 1

---
+++ server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask,jsonify,request
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
+
+load_dotenv()
+
+app = Flask(__name__)
+passw = os.getenv("MONGO_PASS")
+connection_string = f"mongodb+srv://codeomega:{passw}@cluster0.hnyk6mi.mongodb.net/?retryWrites=true&w=majority"
+def MongoDB(collection_name):
+    client = MongoClient(connection_string)
+    db = client.get_database('SIH')
+    collection = db.get_collection(collection_name)
+    return collection
+
+@app.route("/")
+def hello_world():
+    response = {"message": "i am here!!"}
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
