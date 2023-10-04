2eba7913d40aaac84f0a2ba7b714a5689290f54c
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-10-04 18:26:47 +0530

    Added ui for opencv and policeallocation

--- CodeOmega/src/components/CrowdDetection/CrowdDetection.tsx
+++ CodeOmega/src/components/CrowdDetection/CrowdDetection.tsx
@@ -1,7 +1,214 @@
-import React from 'react'
+'use client'
+
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+  Image,
+  Button,
+} from '@chakra-ui/react'
+
+import { Link } from 'react-router-dom';
+import video from "../../assets/video.jpeg"
+import image from "../../assets/image.png"
+import live from "../../assets/live.png"
 
 export default function CrowdDetection() {
+
   return (
-    <div>CrowdDetection</div>
+    <>
+    <Center>
+        <Heading
+          px={{ base: 6, md: 150 }}
+          pb={10}
+          paddingTop={10}
+          fontWeight={'normal'}
+          fontSize={{ base: '3xl', sm: '2xl', md: '3xl' }}
+          lineHeight={'110%'}
+          textAlign="center"
+        >
+          Crowd Detection
+        </Heading>
+      </Center>
+    <Center py={12}>
+      <Box
+        role={'group'}
+        p={6}
+        maxW={'330px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        mr={70} 
+        zIndex={1}>
+        <Box
+          rounded={'lg'}
+          mt={-12}
+          pos={'relative'}
+          height={'270px'}
+          _after={{
+            transition: 'all .3s ease',
+            content: '""',
+            w: 'full',
+            h: 'full',
+            pos: 'absolute',
+            top: 5,
+            left: 0,
+            filter: 'blur(15px)',
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: 'blur(20px)',
+            },
+          }}>
+        <Image
+            rounded={'lg'}
+            height={230}
+            width={282}
+            objectFit={'cover'}
+            src={video}
+            alt="#"
+          />
+        </Box>
+        <Stack pt={10} align={'center'}>
+          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+            Detect Crowd Via
+          </Text>
+          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+            Upload a Video
+          </Heading>
+          <Stack direction={'row'} align={'center'}>
+
+          </Stack>
+        </Stack>
+        <Center>
+          <Button as={Link} to="/complaintcrimes" colorScheme="green" size="lg" my={5} px={6}>
+            Compute
+          </Button>
+        </Center>
+      </Box>
+      <Box
+        role={'group'}
+        p={6}
+        maxW={'330px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        mr={70} 
+        zIndex={1}>
+        <Box
+          rounded={'lg'}
+          mt={-12}
+          pos={'relative'}
+          height={'270px'}
+          _after={{
+            transition: 'all .3s ease',
+            content: '""',
+            w: 'full',
+            h: 'full',
+            pos: 'absolute',
+            top: 5,
+            left: 0,
+            filter: 'blur(15px)',
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: 'blur(20px)',
+            },
+          }}>
+          <Image
+            rounded={'lg'}
+            height={230}
+            width={282}
+            objectFit={'cover'}
+            src={image}
+            alt="#"
+          />
+        </Box>
+        <Stack pt={10} align={'center'}>
+          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+            Detect Crowd Via
+          </Text>
+          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+            Upload an Image
+          </Heading>
+          <Stack direction={'row'} align={'center'}>
+
+          </Stack>
+        </Stack>
+        <Center>
+          <Button as={Link} to="/complaintcleanliness" colorScheme="green" size="lg" my={5} px={6}>
+              Compute
+          </Button>
+        </Center>
+      </Box>
+      <Box
+        role={'group'}
+        p={6}
+        maxW={'330px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.800')}
+        boxShadow={'2xl'}
+        rounded={'lg'}
+        pos={'relative'}
+        mr={70} 
+        zIndex={1}>
+        <Box
+          rounded={'lg'}
+          mt={-12}
+          pos={'relative'}
+          height={'270px'}
+          _after={{
+            transition: 'all .3s ease',
+            content: '""',
+            w: 'full',
+            h: 'full',
+            pos: 'absolute',
+            top: 5,
+            left: 0,
+            filter: 'blur(15px)',
+            zIndex: -1,
+          }}
+          _groupHover={{
+            _after: {
+              filter: 'blur(20px)',
+            },
+          }}>
+          <Image
+            rounded={'lg'}
+            height={230}
+            width={282}
+            objectFit={'cover'}
+            src={live}
+            alt="#"
+          />
+        </Box>
+        <Stack pt={10} align={'center'}>
+          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
+            Detect Crowd Via
+          </Text>
+          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
+            Live Server
+          </Heading>
+          <Stack direction={'row'} align={'center'}>
+
+          </Stack>
+        </Stack>
+        <Center>
+          <Button as={Link} to="/complaintcleanliness" colorScheme="green" size="lg" my={5} px={6}>
+              Compute
+          </Button>
+        </Center>
+      </Box>
+    </Center>
+    </>
   )
 }
