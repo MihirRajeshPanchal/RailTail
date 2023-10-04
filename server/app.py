b057c1f557103d33f569770db4f379b8d3b48ca2
Hatim <hatmsb11@gmail.com>
2023-10-05 02:19:57 +0530

    get tc data

--- server/app.py
+++ server/app.py
@@ -641,19 +641,11 @@ def crowd_detector_image():
     rf = Roboflow(api_key=ROBOFLOW_API_KEY)
     project = rf.workspace().project("crowd_count_v2")
     model = project.version(2).model
-<<<<<<< HEAD
     # print(model.predict(file_path, confidence=40, overlap=30).json())
     model.predict(file_path, confidence=40, overlap=30).save('../CodeOmega/src/components/CrowdDetection/crowd_prediction.jpg')
     response = {"image": "success"}
     print("Response",response)
     return jsonify(response)
-=======
-    results = model.predict(image_file, confidence=40, overlap=30).json()
-    print(results)
-    print('NUMBER OF PEOPLE:', len(results['predictions']))
-    
-    return "done"
->>>>>>> 15123624e7e7bee3b55782052c9009f0cf992014
 
 @app.route("/upload-crowd-video", methods=['POST'])
 def crowd_detector_video():
@@ -749,6 +741,17 @@ def get_clean_complaints():
     print(complaints)
     # Serialize the data to JSON and send it as a response
     return jsonify(complaints)    
+
+@app.route("/get_tc", methods=["GET"])
+def get_tc():
+    # Fetch all documents from the "complaints" collection
+    complaints_collection = MongoDB('tc')
+    complaints = list(complaints_collection.find({}))
+    for complaint in complaints:
+        complaint['_id'] = str(complaint['_id'])
+    print(complaints)
+    # Serialize the data to JSON and send it as a response
+    return jsonify(complaints) 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
